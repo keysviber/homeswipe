95788d8e0cb16b1ba2094398de3e114c176261b4
keysviber <michaelapplix@gmail.com>
2026-06-05 10:35:59 +0200

    updated

---
+++ homeswipe/homeswipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  homeswipe
+//
+//  Created by STRAIGHTEDGE on 3/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ homeswipe/homeswipe/homeswipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeswipeApp.swift
+//  homeswipe
+//
+//  Created by STRAIGHTEDGE on 3/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct homeswipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
